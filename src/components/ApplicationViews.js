c922ad47aa7600e68f0ce7dd776f82b8d7419ea7
Ben Gregory <JBGregoryPro@protonmail.com>
2021-10-27 15:39:23 -0500

    buildout

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import { Route } from "react-router-dom";
 import { Tags } from "./UserViews/tags.js";
 import { Categories } from "./UserViews/categories.js";
-=======
-import React from "react"
-import { Route } from "react-router-dom"
 import { Posts } from "./posts/post"
->>>>>>> main
+
 
 export const ApplicationViews = () => {
     return <>
@@ -15,18 +11,17 @@ export const ApplicationViews = () => {
             margin: "5rem 2rem",
             lineHeight: "1.75rem"
         }}>
-<<<<<<< HEAD
+
             <Route path="/tags">
                 <Tags />
             </Route>
             <Route path="/categories">
                 <Categories />
             </Route>
-=======
-        <Route path= "/posts">
-            <Posts />
-        </Route>
->>>>>>> main
+            <Route path= "/posts">
+                <Posts />
+            </Route>
+
         </main>
     </>
 }
