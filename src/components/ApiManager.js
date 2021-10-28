d3db43efead564fbedd97a1fe17e0ba66a224801
Kylie Postlethwaite <81774908+kyliepost@users.noreply.github.com>
2021-10-28 13:24:36 -0500

    Update ApiManager.js

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -3,12 +3,12 @@ export const getAllPosts = () => {
         .then(res => res.json())
 }
 
-<<<<<<< HEAD
+
 export const getAllTags = () => {
     return fetch("http://localhost:8088/tags")
         .then(res => res.json())
 }
-=======
+
 export const getAllCategories = () => {
     return fetch("http://localhost:8088/categories")
         .then(res => res.json())
@@ -20,4 +20,4 @@ export const getAllCategories = () => {
 //         .then(res => res.json())
 //         .then(data => setCategoryId(data.id));
 // }
->>>>>>> ab6ab35018da3685d17f28ad2511f58f563de161
+
