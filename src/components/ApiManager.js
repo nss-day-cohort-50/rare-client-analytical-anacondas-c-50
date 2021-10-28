8f6205555e774d179cf5cfb8a8852b0ee9f2d4d6
Alicia AyersLott <amayers92@gmail.com>
2021-10-28 12:01:25 -0500

    rendering categories

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -2,3 +2,15 @@ export const getAllPosts = () => {
     return fetch("http://localhost:8088/posts")
         .then(res => res.json())
 }
+
+export const getAllCategories = () => {
+    return fetch("http://localhost:8088/categories")
+        .then(res => res.json())
+}
+
+// export const setNewCategory = () => {
+//     return fetch("http://localhost:8088/categories", {
+//         method: "POST" })
+//         .then(res => res.json())
+//         .then(data => setCategoryId(data.id));
+// }
--- src/components/UserViews/categories.js
+++ src/components/UserViews/categories.js
@@ -1,14 +1,56 @@
 import React from "react"
+import { useEffect, useState } from "react"
 import { Route } from "react-router-dom"
 import "./categories.css"
+import { getAllCategories, getAllPosts } from "../ApiManager"
+
 
 export const Categories = () => {
+    const [categories, setCategories] = useState([])
+    // const [newCategories, setNew] = useState([])
+
+    useEffect(
+        () => {
+                getAllCategories()
+                .then((data) => {
+                    setCategories(data)
+                })
+        },
+        []
+    )
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(
+        () => {
+                getAllPosts()
+                .then((data) => {
+                    setPosts(data)
+                })
+        },
+        []
+    )
+
+    console.log(categories)
+    // useEffect(() => {
+    //     // POST request using fetch inside useEffect React hook
+    //     const newCategory = {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify({ label: '' })
+    //     };
+    //     setNewCategory(setNew)
+    
+    // // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    // }, []);
+    // // console.log(categories)
+    // console.log(newCategories)
 
     return (
         <>
         <div><h1 className="header">Categories</h1>
-        <div className="catList"><ul><li className="cat">Cool Categories Bro</li></ul></div></div>
-
+        {categories.map((category) => { 
+        return <div className="catList"><ul><li className="cat">{category.label}</li></ul></div>})}</div>
         <div className="catBox newCat"><h3>Create New Category</h3>
         <div>
             </div><input type="text" placeholder="A cool new category..."></input><button>Submit</button></div>
