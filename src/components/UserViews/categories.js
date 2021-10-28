166ec1def6d75f5ce86c69e3157a3741af93b584
Alicia AyersLott <amayers92@gmail.com>
2021-10-28 13:13:24 -0500

    cleaned up code to show categories

--- src/components/UserViews/categories.js
+++ src/components/UserViews/categories.js
@@ -2,16 +2,15 @@ import React from "react"
 import { useEffect, useState } from "react"
 import { Route } from "react-router-dom"
 import "./categories.css"
-import { getAllCategories, getAllPosts } from "../ApiManager"
+import { getAllCategories } from "../ApiManager"
 
 
 export const Categories = () => {
     const [categories, setCategories] = useState([])
-    // const [newCategories, setNew] = useState([])
 
     useEffect(
         () => {
-                getAllCategories()
+            getAllCategories()
                 .then((data) => {
                     setCategories(data)
                 })
@@ -19,42 +18,16 @@ export const Categories = () => {
         []
     )
 
-    const [posts, setPosts] = useState([])
-
-    useEffect(
-        () => {
-                getAllPosts()
-                .then((data) => {
-                    setPosts(data)
-                })
-        },
-        []
-    )
-
-    console.log(categories)
-    // useEffect(() => {
-    //     // POST request using fetch inside useEffect React hook
-    //     const newCategory = {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({ label: '' })
-    //     };
-    //     setNewCategory(setNew)
-    
-    // // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    // }, []);
-    // // console.log(categories)
-    // console.log(newCategories)
-
     return (
         <>
-        <div><h1 className="header">Categories</h1>
-        {categories.map((category) => { 
-        return <div className="catList"><ul><li className="cat">{category.label}</li></ul></div>})}</div>
-        <div className="catBox newCat"><h3>Create New Category</h3>
-        <div>
-            </div><input type="text" placeholder="A cool new category..."></input><button>Submit</button></div>
-            
+            <div><h1 className="header">Categories</h1>
+                {categories.map((category) => {
+                    return <div className="catList"><ul><li className="cat">{category.label}</li></ul></div>
+                })}</div>
+            <div className="catBox newCat"><h3>Create New Category</h3>
+                <div>
+                </div><input type="text" placeholder="A cool new category..."></input><button>Submit</button></div>
+
         </>
     )
 }
